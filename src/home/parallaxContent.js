0035e401b1196c8907836f39751daf989330b71a
Faklor <faklor_br_ar@mail.ru>
2023-12-09 15:01:59 +0300

    addContentParallax

---
+++ src/home/parallaxContent.js
@@ -0,0 +1,9 @@
+import './scss/parallaxContent.scss'
+
+export default function ParallaxContent(){
+    return(
+        <div className='parallax'>
+            <h1>Hello Alisa</h1>
+        </div>
+    )
+}
